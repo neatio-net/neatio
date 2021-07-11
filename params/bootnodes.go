063b56ee8e0a0e503f51de3f794233e134c4840e
GSilviu <77549271+GSilviu@users.noreply.github.com>
2021-07-11 08:39:45 +0300

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -18,14 +18,11 @@ package params
 
 // MainnetBootnodes are the enode URLs of the P2P bootstrap nodes running on
 // the Neatio main network.
-var MainnetBootnodes = []string{
-
-	"enode://7eb31644b1287035867410f477eef6b069dee6226b682cb600b38be977c9f26152901f73c53dbed86151286a17f126e17f0d371d656b98e6f878733c045b1f7a@135.181.154.74:9910",
-	"enode://2ecd139bfee3d5cfeaa352be010ea1fe2f6cb3a5f58528c68e9fbfb76069691cc2444e85ea78542d042ccdcd1b3048f55f68f893ce22508b7bcf9ad10ecbcbb4@109.166.131.109:9910",
-}
+var MainnetBootnodes = []string{}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
 // Neatio test network.
 var TestnetBootnodes = []string{
-	"enode://f4caa1a33b8740093103b3866a42f51d55c65c314cfb81328a20968fc58f9c489d14a49f88a60a60d1193a3f4fdaeb1e51b3b1f83c51ed920e34853e05be9329@127.0.0.1:9911",
+	"enode://b0d0db3876616f19f80417950ea8d43483611754bc564797d9048bf64953f0e20410fed5583b8aeca34778563d1480724b33427187afdb06344cfa8ec863c056@135.181.195.79:9910",
+	"enode://b0d0db3876616f19f80417950ea8d43483611754bc564797d9048bf64953f0e20410fed5583b8aeca34778563d1480724b33427187afdb06344cfa8ec863c056@135.181.195.79:9911",
 }
