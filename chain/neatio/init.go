aa832b0e3996947056d71299fdcccb0dd66bb2be
neatChronos <adimunteanu88@gmail.com>
2021-08-26 19:59:56 +0300

    Rename

--- chain/neatio/init.go
+++ chain/neatio/init.go
@@ -304,7 +304,7 @@ func createGenesisDoc(config cfg.Config, chainId string, coreGenesis *core.Genes
 
 		var rewardPerBlock *big.Int
 		if chainId == MainChain || chainId == TestnetChain {
-			rewardPerBlock = big.NewInt(7900000000000000) // 0.079 NEAT per block
+			rewardPerBlock = big.NewInt(7900000000000000) // 0.079 NEAT
 		} else {
 			rewardPerBlock = big.NewInt(0)
 		}
