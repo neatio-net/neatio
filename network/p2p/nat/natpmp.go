fa75b8d685d0adce706461b2ff0f7e3efeba092e
neatChronos <adimunteanu88@gmail.com>
2022-01-09 21:05:23 +0200

    Code cleanup

--- network/p2p/nat/natpmp.go
+++ network/p2p/nat/natpmp.go
@@ -1,19 +1,3 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package nat
 
 import (
@@ -22,11 +6,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/jackpal/go-nat-pmp"
+	natpmp "github.com/jackpal/go-nat-pmp"
 )
 
-// natPMPClient adapts the NAT-PMP protocol implementation so it conforms to
-// the common interface.
 type pmp struct {
 	gw net.IP
 	c  *natpmp.Client
@@ -48,22 +30,19 @@ func (n *pmp) AddMapping(protocol string, extport, intport int, name string, lif
 	if lifetime <= 0 {
 		return fmt.Errorf("lifetime must not be <= 0")
 	}
-	// Note order of port arguments is switched between our
-	// AddMapping and the client's AddPortMapping.
+
 	_, err := n.c.AddPortMapping(strings.ToLower(protocol), intport, extport, int(lifetime/time.Second))
 	return err
 }
 
 func (n *pmp) DeleteMapping(protocol string, extport, intport int) (err error) {
-	// To destroy a mapping, send an add-port with an internalPort of
-	// the internal port to destroy, an external port of zero and a
-	// time of zero.
+
 	_, err = n.c.AddPortMapping(strings.ToLower(protocol), intport, 0, 0)
 	return err
 }
 
 func discoverPMP() Interface {
-	// run external address lookups on all potential gateways
+
 	gws := potentialGateways()
 	found := make(chan *pmp, len(gws))
 	for i := range gws {
@@ -77,9 +56,7 @@ func discoverPMP() Interface {
 			}
 		}()
 	}
-	// return the one that responds first.
-	// discovery needs to be quick, so we stop caring about
-	// any responses after a very short timeout.
+
 	timeout := time.NewTimer(1 * time.Second)
 	defer timeout.Stop()
 	for range gws {
@@ -96,14 +73,11 @@ func discoverPMP() Interface {
 }
 
 var (
-	// LAN IP ranges
 	_, lan10, _  = net.ParseCIDR("10.0.0.0/8")
 	_, lan176, _ = net.ParseCIDR("172.16.0.0/12")
 	_, lan192, _ = net.ParseCIDR("192.168.0.0/16")
 )
 
-// TODO: improve this. We currently assume that (on most networks)
-// the router is X.X.X.1 in a local LAN range.
 func potentialGateways() (gws []net.IP) {
 	ifaces, err := net.Interfaces()
 	if err != nil {
--- network/p2p/netutil/error_test.go
+++ network/p2p/netutil/error_test.go
@@ -1,19 +1,3 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package netutil
 
 import (
@@ -22,9 +6,6 @@ import (
 	"time"
 )
 
-// This test checks that isPacketTooBig correctly identifies
-// errors that result from receiving a UDP packet larger
-// than the supplied receive buffer.
 func TestIsPacketTooBig(t *testing.T) {
 	listener, err := net.ListenPacket("udp", "127.0.0.1:0")
 	if err != nil {
