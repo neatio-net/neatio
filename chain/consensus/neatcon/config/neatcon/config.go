2485327dd836d1b322a5914bf520d9b9667aeb1a
ad-imun <adimun.mail@gmail.com>
2022-03-29 21:54:56 +0300

    Changed block-space to 5 seconds

--- chain/consensus/neatcon/config/neatcon/config.go
+++ chain/consensus/neatcon/config/neatcon/config.go
@@ -89,10 +89,10 @@ func GetConfig(rootDir, chainId string) cfg.Config {
 	mapConfig.SetDefault("timeout_propose", 1500)
 	mapConfig.SetDefault("timeout_propose_delta", 500)
 	mapConfig.SetDefault("timeout_prevote", 2000)
-	mapConfig.SetDefault("timeout_prevote_delta", 1000)
+	mapConfig.SetDefault("timeout_prevote_delta", 500)
 	mapConfig.SetDefault("timeout_precommit", 2000)
-	mapConfig.SetDefault("timeout_precommit_delta", 1000)
-	mapConfig.SetDefault("timeout_commit", 10000) // 10 sec
+	mapConfig.SetDefault("timeout_precommit_delta", 500)
+	mapConfig.SetDefault("timeout_commit", 5000) // 5 sec
 
 	mapConfig.SetDefault("skip_timeout_commit", false)
 	mapConfig.SetDefault("mempool_recheck", true)
