51486f99e63d5d9df0df16f4c9dc95f8416e471e
MrTizu <99123684+tizul@users.noreply.github.com>
2023-09-24 22:47:16 +0300

    Update solc.go

--- internal/neatapi/solc.go
+++ internal/neatapi/solc.go
@@ -1,19 +1,3 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package neatapi
 
 import (
@@ -48,16 +32,12 @@ func makeCompilerAPIs(solcPath string) []rpc.API {
 }
 
 type compilerAPI struct {
-	// This lock guards the solc path set through the API.
-	// It also ensures that only one solc process is used at
-	// any time.
 	mu   sync.Mutex
 	solc string
 }
 
 type CompilerAdminAPI compilerAPI
 
-// SetSolc sets the Solidity compiler path to be used by the node.
 func (api *CompilerAdminAPI) SetSolc(path string) (string, error) {
 	api.mu.Lock()
 	defer api.mu.Unlock()
@@ -71,7 +51,6 @@ func (api *CompilerAdminAPI) SetSolc(path string) (string, error) {
 
 type PublicCompilerAPI compilerAPI
 
-// CompileSolidity compiles the given solidity source.
 func (api *PublicCompilerAPI) CompileSolidity(source string) (map[string]*compiler.Contract, error) {
 	api.mu.Lock()
 	defer api.mu.Unlock()
