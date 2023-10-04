0d1c39f7623ea13b0fd25834d7097d0007bebe26
BlackSea <dev@neatio.net>
2023-10-04 22:40:12 +0300

    cleaning up code

--- tests/init.go
+++ tests/init.go
@@ -1,19 +1,3 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package tests
 
 import (
@@ -23,7 +7,6 @@ import (
 	"github.com/nio-net/nio/params"
 )
 
-// This table defines supported forks and their chain config.
 var Forks = map[string]*params.ChainConfig{
 	"Frontier": {
 		ChainId: big.NewInt(1),
@@ -78,7 +61,6 @@ var Forks = map[string]*params.ChainConfig{
 	},
 }
 
-// UnsupportedForkError is returned when a test requests a fork that isn't implemented.
 type UnsupportedForkError struct {
 	Name string
 }
