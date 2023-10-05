41426357dd718b88b3749c25494dae65598c9c5d
BlackSea <dev@neatio.net>
2023-10-05 21:22:32 +0300

    cleaning up code

--- tests/transaction_test_util.go
+++ tests/transaction_test_util.go
@@ -1,19 +1,3 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package tests
 
 import (
@@ -30,7 +14,6 @@ import (
 	"github.com/nio-net/nio/utilities/rlp"
 )
 
-// TransactionTest checks RLP decoding and sender derivation of transactions.
 type TransactionTest struct {
 	json ttJSON
 }
@@ -76,7 +59,6 @@ func (tt *TransactionTest) Run(config *params.ChainConfig) error {
 			return fmt.Errorf("RLP decoding failed: %v", err)
 		}
 	}
-	// Check sender derivation.
 	signer := types.MakeSigner(config, new(big.Int).SetUint64(uint64(tt.json.BlockNumber)))
 	sender, err := types.Sender(signer, tx)
 	if err != nil {
@@ -85,7 +67,6 @@ func (tt *TransactionTest) Run(config *params.ChainConfig) error {
 	if sender != common.BytesToAddress(tt.json.Sender) {
 		return fmt.Errorf("Sender mismatch: got %x, want %x", sender, tt.json.Sender)
 	}
-	// Check decoded fields.
 	err = tt.json.Transaction.verify(signer, tx)
 	if tt.json.Sender == nil && err == nil {
 		return errors.New("field validations succeeded but should fail")
