2b6284b8eb9e0df38a4fbce77c00b1ecb552ae0f
BlackSea <dev@neatio.net>
2023-10-05 21:24:46 +0300

    cleaning up code

--- tests/rlp_test_util.go
+++ tests/rlp_test_util.go
@@ -1,19 +1,3 @@
-// Copyright 2015 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package tests
 
 import (
@@ -27,34 +11,22 @@ import (
 	"github.com/nio-net/nio/utilities/rlp"
 )
 
-// RLPTest is the JSON structure of a single RLP test.
 type RLPTest struct {
-	// If the value of In is "INVALID" or "VALID", the test
-	// checks whether Out can be decoded into a value of
-	// type interface{}.
-	//
-	// For other JSON values, In is treated as a driver for
-	// calls to rlp.Stream. The test also verifies that encoding
-	// In produces the bytes in Out.
 	In interface{}
 
-	// Out is a hex-encoded RLP value.
 	Out string
 }
 
-// Run executes the test.
 func (t *RLPTest) Run() error {
 	outb, err := hex.DecodeString(t.Out)
 	if err != nil {
 		return fmt.Errorf("invalid hex in Out")
 	}
 
-	// Handle simple decoding tests with no actual In value.
 	if t.In == "VALID" || t.In == "INVALID" {
 		return checkDecodeInterface(outb, t.In == "VALID")
 	}
 
-	// Check whether encoding the value produces the same bytes.
 	in := translateJSON(t.In)
 	b, err := rlp.EncodeToBytes(in)
 	if err != nil {
@@ -63,7 +35,6 @@ func (t *RLPTest) Run() error {
 	if !bytes.Equal(b, outb) {
 		return fmt.Errorf("encode produced %x, want %x", b, outb)
 	}
-	// Test stream decoding.
 	s := rlp.NewStream(bytes.NewReader(outb), 0)
 	return checkDecodeFromJSON(s, in)
 }
@@ -79,13 +50,12 @@ func checkDecodeInterface(b []byte, isValid bool) error {
 	return nil
 }
 
-// translateJSON makes test json values encodable with RLP.
 func translateJSON(v interface{}) interface{} {
 	switch v := v.(type) {
 	case float64:
 		return uint64(v)
 	case string:
-		if len(v) > 0 && v[0] == '#' { // # starts a faux big int.
+		if len(v) > 0 && v[0] == '#' {
 			big, ok := new(big.Int).SetString(v[1:], 10)
 			if !ok {
 				panic(fmt.Errorf("bad test: bad big int: %q", v))
@@ -104,10 +74,6 @@ func translateJSON(v interface{}) interface{} {
 	}
 }
 
-// checkDecodeFromJSON decodes from s guided by exp. exp drives the
-// Stream by invoking decoding operations (Uint, Big, List, ...) based
-// on the type of each value. The value decoded from the RLP stream
-// must match the JSON value.
 func checkDecodeFromJSON(s *rlp.Stream, exp interface{}) error {
 	switch exp := exp.(type) {
 	case uint64:
