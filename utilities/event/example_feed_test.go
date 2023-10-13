6ac161a374fbb4b347a7917749357b458843d833
BlackSea <dev@neatio.net>
2023-10-13 20:06:16 +0300

    cleaning up code

--- utilities/event/example_feed_test.go
+++ utilities/event/example_feed_test.go
@@ -1,19 +1,3 @@
-// Copyright 2016 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package event_test
 
 import (
@@ -23,15 +7,12 @@ import (
 )
 
 func ExampleFeed_acknowledgedEvents() {
-	// This example shows how the return value of Send can be used for request/reply
-	// interaction between event consumers and producers.
 	var feed event.Feed
 	type ackedEvent struct {
 		i   int
 		ack chan<- struct{}
 	}
 
-	// Consumers wait for events on the feed and acknowledge processing.
 	done := make(chan struct{})
 	defer close(done)
 	for i := 0; i < 3; i++ {
@@ -51,8 +32,6 @@ func ExampleFeed_acknowledgedEvents() {
 		}()
 	}
 
-	// The producer sends values of type ackedEvent with increasing values of i.
-	// It waits for all consumers to acknowledge before sending the next event.
 	for i := 0; i < 3; i++ {
 		acksignal := make(chan struct{})
 		n := feed.Send(ackedEvent{i, acksignal})
@@ -60,14 +39,4 @@ func ExampleFeed_acknowledgedEvents() {
 			<-acksignal
 		}
 	}
-	// Output:
-	// 0
-	// 0
-	// 0
-	// 1
-	// 1
-	// 1
-	// 2
-	// 2
-	// 2
 }
