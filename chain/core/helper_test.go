e5247b21dfcdec29e3952c6401b3c32d23515b9c
BlackSea <dev@neatio.net>
2023-10-25 21:16:26 +0300

    cleaning up code

--- chain/core/helper_test.go
+++ chain/core/helper_test.go
@@ -1,19 +1,3 @@
-// Copyright 2014 The go-ethereum Authors
-// This file is part of the go-ethereum library.
-//
-// The go-ethereum library is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The go-ethereum library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
-
 package core
 
 import (
@@ -25,9 +9,7 @@ import (
 	"github.com/nio-net/nio/utilities/event"
 )
 
-// Implement our EthTest Manager
 type TestManager struct {
-	// stateManager *StateManager
 	eventMux *event.TypeMux
 
 	db         neatdb.Database
@@ -60,35 +42,19 @@ func (tm *TestManager) TxPool() *TxPool {
 	return tm.txPool
 }
 
-// func (tm *TestManager) StateManager() *StateManager {
-// 	return tm.stateManager
-// }
-
 func (tm *TestManager) EventMux() *event.TypeMux {
 	return tm.eventMux
 }
-
-// func (tm *TestManager) KeyManager() *crypto.KeyManager {
-// 	return nil
-// }
 
 func (tm *TestManager) Db() neatdb.Database {
 	return tm.db
 }
 
 func NewTestManager() *TestManager {
-	//db, err := rawdb.NewMemoryDatabase()
-	//if err != nil {
-	//	fmt.Println("Could not create mem-db, failing")
-	//	return nil
-	//}
 
 	testManager := &TestManager{}
 	testManager.eventMux = new(event.TypeMux)
 	testManager.db = rawdb.NewMemoryDatabase()
-	// testManager.txPool = NewTxPool(testManager)
-	// testManager.blockChain = NewBlockChain(testManager)
-	// testManager.stateManager = NewStateManager(testManager)
 
 	return testManager
 }
